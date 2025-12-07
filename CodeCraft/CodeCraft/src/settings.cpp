a32f07c2b46a801391f38edf6ca8717346f8c86e
Dimitar Bornusuzov <DGBornusuzov24@codingburgas.bg>
2025-12-07 12:00:57 +0200

    Change the color of the Settings menu (Close #24)

--- CodeCraft/CodeCraft/src/settings.cpp
+++ CodeCraft/CodeCraft/src/settings.cpp
@@ -13,7 +13,14 @@ bool hintSystemOn = true;
 
 void showSettings() {
     clearScreen();
+    
     WORD cyan = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+    WORD red = FOREGROUND_RED | FOREGROUND_INTENSITY;
+    WORD green = FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+    WORD yellow = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+    WORD blue = FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+    WORD purple = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+
     int width, height;
     getConsoleSize(width, height);
     int startY = (height - 18) / 2;
@@ -27,21 +34,21 @@ void showSettings() {
         y += 8;
 
         printCenteredAt("+============================================================+", y++, cyan);
-        printCenteredAt("|                 >>> S E T T I N G S <<<                   |", y++, cyan);
+        printCenteredAt("|                 >>> S E T T I N G S <<<                   |", y++, yellow);
         printCenteredAt("+============================================================+", y++, cyan);
         y += 2;
 
         string diffTxt = (difficulty == 0 ? "Easy" :
             difficulty == 1 ? "Medium" : "Hard");
 
-        printCenteredAt(("1) Sound Effects: " + string(soundOn ? "ON" : "OFF")).c_str(), y++, cyan);
-        printCenteredAt(("2) Story Mode: " + string(storyModeOn ? "Enabled" : "Disabled")).c_str(), y++, cyan);
+        printCenteredAt(("1) Sound Effects: " + string(soundOn ? "ON" : "OFF")).c_str(), y++, green);
+        printCenteredAt(("2) Story Mode: " + string(storyModeOn ? "Enabled" : "Disabled")).c_str(), y++, blue);
         printCenteredAt(("3) Timer: " + string(timerOn ? "Enabled" : "Disabled")).c_str(), y++, cyan);
-        printCenteredAt(("4) Difficulty: " + diffTxt).c_str(), y++, cyan);
-        printCenteredAt(("5) Hint System: " + string(hintSystemOn ? "Available" : "Disabled")).c_str(), y++, cyan);
+        printCenteredAt(("4) Difficulty: " + diffTxt).c_str(), y++, red);
+        printCenteredAt(("5) Hint System: " + string(hintSystemOn ? "Available" : "Disabled")).c_str(), y++, yellow);
 
         y += 2;
-        printCenteredAt("Press 1-5 to change settings, ESC to return...", y, cyan);
+        printCenteredAt("Press 1-5 to change settings, ESC to return...", y, purple);
 
         int key = _getch();
 
